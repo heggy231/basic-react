26ddbff73629b830896c63df28a0008bbb62e7d4
Heggyhere <heggyy@gmail.com>
2019-04-29 11:19:37 -0700

    when directly inpu html jsx doens't like it

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,7 +4,17 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-  return <div>Hi There!</div>
+  return (
+  <div>
+    <div>
+      {/* label attribute for="name" associate to input with id="name" 
+      what this does when click on label > cursor goes inside name input field  */}
+      <label class="label" for="name">Enter name:</label>
+      <input id="name" type="text"/>
+      <button style="background-color: blue; color: white;">Submit</button>
+    </div>
+  </div>
+  );
 };
 // Take the react component and show it on the screen
 ReactDOM.render(
