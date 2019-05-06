c361d03fb27a9da78da906442fc769657c01120e
Heggyhere <heggyy@gmail.com>
2019-05-06 10:01:02 -0700

    see you can add more new commment detail by just adding       <CommentDetail />

--- components/src/index.js
+++ components/src/index.js
@@ -7,7 +7,9 @@ const App = () => {
   return (
     <div className="ui container comments">
       <CommentDetail />
-
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
     </div>
   );
 };
