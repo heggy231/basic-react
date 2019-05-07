b7fd20425a67a3509a841d9440542046ae4841e6
Heggyhere <heggyy@gmail.com>
2019-05-07 12:42:20 -0700

    conver hard code data into props

--- components/src/CommentDetail.js
+++ components/src/CommentDetail.js
@@ -6,16 +6,16 @@ const CommentDetail = props => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()}/>
+        <img alt="avatar" src={props.avatarImg}/>
       </a>
       <div className="content">
         <a href="/" className="author">
-          Sam
+          {props.author}
         </a>
         <div className="metadata">
-          <span className="date">Today at 6:00PM</span>
+          <span className="date">{props.timeAgo}</span>
         </div>
-        <div className="text">Nice blog post!</div>
+        <div className="text">{props.userComment}</div>
       </div>
     </div>
   );
--- components/src/index.js
+++ components/src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail 
+        avatarImg={faker.image.avatar()} 
+        author="Sam" 
+        timeAgo="Friday at 12:00 PM"
+        userComment="Nice blog post!"
+      />
+      <CommentDetail 
+        avatarImg={faker.image.avatar()} 
+        author="Alex" 
+        timeAgo="Saturday at 6:00 PM"
+        userComment="I am signing up now."
+      />
+      <CommentDetail 
+        avatarImg={faker.image.avatar()} 
+        author="Jane" 
+        timeAgo="Saturday at 9:00 PM"
+        userComment="Let's start a meetup!"
+      />
     </div>
   );
 };
