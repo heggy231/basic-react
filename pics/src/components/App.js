4d2db28f338444f8aa1adf654805573926cbad57
Heggy Castaneda <heggy_castaneda@gap.com>
2019-06-18 11:47:35 -0700

    async await

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -6,25 +6,23 @@ import SearchBar from './SearchBar';
 // functional const App = () => { return <div> }
 // class based component
 class App extends React.Component {
-  onSearchSubmit(term) {
+  async onSearchSubmit(term) {
     // api end point url and config optional argument
-    axios.get('https://api.unsplash.com/search/photos', {
-      // https://unsplash.com/documentation#parameters-11
-      // what happens with params adds to the end of url https://api.unsplash.com/search/photos?yourQueryTermValue  ex) https://api.unsplash.com/search/photos?car
-      // Request Config for Axios https://github.com/axios/axios#request-config
-      params: { query: term },
-      // header doc https://unsplash.com/documentation#public-actions
-      headers: {
+    // await key word for anything that may makes network request
+    const response = await axios
+      .get('https://api.unsplash.com/search/photos', {
+        // https://unsplash.com/documentation#parameters-11
+        // what happens with params adds to the end of url https://api.unsplash.com/search/photos?yourQueryTermValue  ex) https://api.unsplash.com/search/photos?car
+        // Request Config for Axios https://github.com/axios/axios#request-config
+        params: { query: term },
+        // header doc https://unsplash.com/documentation#public-actions
+        headers: {
         //Authorization: Client-ID YOUR_ACCESS_KEY
-        Authorization: 'Client-ID c60a2d465519232c93936ce432ae69a1d052aa60ee8dc5b01a22919ef699590c'
-      }
-    // after asynch axios network request promise obj get passed, arrow function is cb invoked with data we got back splash api
-    }).then((response) => {
-      // debugger;
-      // console.log(response);
-      // URL Regular for its path
-      console.log(response.data.results[0].urls.regular);
-    });
+          Authorization: 'Client-ID c60a2d465519232c93936ce432ae69a1d052aa60ee8dc5b01a22919ef699590c'
+        }
+      // after asynch axios network request promise obj get passed, arrow function is cb invoked with data we got back splash app
+      });
+    console.log(response.data.results);
   }
   render() {
     return (
